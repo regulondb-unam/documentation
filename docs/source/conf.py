ae759fb4efcfe69fb19ea6277fa45aa25fd1483e
Computational-Genomics-Research-Program <adminpgc@ccg.unam.mx>
2019-08-20 11:51:51 -0500

    Add Sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+html_theme = "sphinx_rtd_theme"
 
 
 # -- Project information -----------------------------------------------------
